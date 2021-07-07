c2406dde05686b624600620b9bcb8099941cb97f
Manisha Goyal <manisha.goyal@allscripts.com>
2021-07-07 11:13:32 +0530

    initial commit

---
+++ DjangoWebProject7/project7_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\M819825\source\repos\DjangoWebProject7\DjangoWebProject7\project7_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
